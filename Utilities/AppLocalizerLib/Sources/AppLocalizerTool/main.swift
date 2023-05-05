c74bb94fcc7b67a40bd10b7c435f4ad065db9b73
marc-medley <marc-medley@users.noreply.github.com>
2023-05-05 03:02:27 -0700

    script updates for changesets 01/02/03

--- Utilities/AppLocalizerLib/Sources/AppLocalizerTool/main.swift
+++ Utilities/AppLocalizerLib/Sources/AppLocalizerTool/main.swift
@@ -79,7 +79,6 @@ let BatchSubdir = "batch_normal"
 //let BatchCommands = "batch_normal_intake_B.txt"        // *B*aseline
 //let BatchCommands = "batch_normal_intake_C.txt"        // *C*hangeSet
 //let BatchCommands = "batch_normal_intake_C_1.txt"      // *C*hangeSet01
-//let BatchCommands = "batch_normal_intake_C_1.txt"      // *C*hangeSet01
 //let BatchCommands = "batch_normal_intake_C_2.txt"      // *C*hangeSet02
 //let BatchCommands = "batch_normal_intake_C_3.txt"      // *C*hangeSet03
 //let BatchCommands = "batch_normal_intake+BC.txt"       // *B*aseline + *C*hangeSet
