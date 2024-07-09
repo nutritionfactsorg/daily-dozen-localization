226ff0c04784eb78bbebb4fd170442f4e685fa4c
marc-medley <marc-medley@users.noreply.github.com>
2024-07-09 14:53:59 -0700

    Update Ukrainian (uk) for migration to applications.

--- Utilities/AppLocalizerLib/Sources/AppLocalizerTool/main.swift
+++ Utilities/AppLocalizerLib/Sources/AppLocalizerTool/main.swift
@@ -89,14 +89,14 @@ let BatchSubdir = "batch_normal"
 //let BatchCommands = "batch_normal_intake__it_v03.txt"
 //let BatchCommands = "batch_normal_intake__fr_v04.txt"
 //let BatchCommands = "batch_normal_intake__ro.txt"
-let BatchCommands = "batch_normal_intake__uk_v05.txt" // Ukrainian
+//let BatchCommands = "batch_normal_intake__uk_v05.txt" // Ukrainian
 //let BatchCommands = "batch_normal_intake__zh_v05.txt"
 //let BatchCommands = "batch_normal_intake_B.txt"        // *B*aseline
 //let BatchCommands = "batch_normal_intake_C.txt"        // *C*hangeSet
 //let BatchCommands = "batch_normal_intake_C_1.txt"      // *C*hangeSet01
 //let BatchCommands = "batch_normal_intake_C_2.txt"      // *C*hangeSet02
 //let BatchCommands = "batch_normal_intake_C_3.txt"      // *C*hangeSet03
-//let BatchCommands = "batch_normal_intake+BC.txt"       // *B*aseline + *C*hangeSet
+let BatchCommands = "batch_normal_intake+BC.txt"       // *B*aseline + *C*hangeSet
 //let BatchCommands = "batch_normal_intake+BC_1…4.txt"   // *B*aseline + *C*hangeSet
 //let BatchCommands = "batch_normal_intake+I_5.txt"      // *I*nset
 
